a14d010a619e4978341438b1243002b4b3ae242a
yuus95 <kkad45@naver.com>
2021-09-30 18:51:49 +0900

    [init] git Project 초기화

---
+++ src/main/java/com/market/simple/yuus_market/YuusMarketApplication.java
@@ -0,0 +1,13 @@
+package com.market.simple.yuus_market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YuusMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YuusMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/market/simple/yuus_market/YuusMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.market.simple.yuus_market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YuusMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
