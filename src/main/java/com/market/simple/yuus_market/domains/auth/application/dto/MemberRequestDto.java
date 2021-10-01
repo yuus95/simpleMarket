fea447ae81ab12a7d087caff5003e61878675d43
yuus95 <kkad45@naver.com>
2021-10-01 10:16:33 +0900

    [add] 회원가입 관련 Dto 구현 MemberRequestDto.java - 회원가입시 들어오는 데이터 관련 Dto생성 MemberResponseDto.java - 회원가입 완료후 반환 Dto생성

---
+++ src/main/java/com/market/simple/yuus_market/domains/auth/application/dto/MemberRequestDto.java
@@ -0,0 +1,49 @@
+package com.market.simple.yuus_market.domains.auth.application.dto;
+
+
+import com.market.simple.yuus_market.domains.member.domain.AuthProvider;
+import com.market.simple.yuus_market.domains.member.domain.Authority;
+import com.market.simple.yuus_market.domains.member.domain.Member;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class MemberRequestDto {
+
+
+    @NotBlank(message = "닉네임은 필수 입니다.")
+    private String nickname;
+
+
+    @NotBlank(message = "이메일은 필수입니다.")
+    @Email(message = "올바른 이메일 형식을 입력해주세요")
+    private String userId;
+    @NotBlank(message = "비밀번호는 필수입니다.")
+    private String password;
+
+    @NotBlank(message = "핸드폰번호는 필수입니다.")
+    @Pattern(regexp ="^01(?:0|1|[6-9])-(?:\\d{3}|\\d{4})-\\d{4}$",message = "xxx-xxxx-xxxx 형식을 맞춰주세요")
+    private String phone;
+
+
+
+
+    public Member toMember(PasswordEncoder passwordEncoder) {
+        return Member.builder()
+                .nickname(nickname)
+                .userId(userId)
+                .password(passwordEncoder.encode(password))
+                .phone(phone)
+                .authority(Authority.ROLE_USER)
+                .provider(AuthProvider.local)
+                .build();
+    }
+}
---
+++ src/main/java/com/market/simple/yuus_market/domains/auth/application/dto/MemberResponseDto.java
@@ -0,0 +1,20 @@
+package com.market.simple.yuus_market.domains.auth.application.dto;
+
+import com.market.simple.yuus_market.domains.member.domain.Member;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * 저장했을 경우 userID 반환
+ */
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class MemberResponseDto {
+    private String email;
+
+    public static MemberResponseDto of(Member member) {
+        return new MemberResponseDto(member.getUserId());
+    }
+}
