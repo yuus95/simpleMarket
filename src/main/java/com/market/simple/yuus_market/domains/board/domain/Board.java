f7d2eaa919d96ee8dd09a054e874bae439405f0f
yuus95 <kkad45@naver.com>
2021-10-01 17:20:07 +0900

    [add] 보드도메인 사진 관련 속성 변경 Board.java - 속성 변경 및 빌더 생성

--- src/main/java/com/market/simple/yuus_market/domains/board/domain/Board.java
+++ src/main/java/com/market/simple/yuus_market/domains/board/domain/Board.java
@@ -2,14 +2,19 @@ package com.market.simple.yuus_market.domains.board.domain;
 
 import com.market.simple.yuus_market.domains.baseEntity;
 import com.market.simple.yuus_market.domains.member.domain.Member;
+import com.market.simple.yuus_market.domains.photo.domain.Photo;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @NoArgsConstructor
 @Getter
 @Entity
+@Table(name = "boards")
 public class Board extends baseEntity {
 
     @Id
@@ -19,12 +24,36 @@ public class Board extends baseEntity {
     private String content;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "memberId")
+    @JoinColumn(name = "member_Id")
     private Member member;
+
+    @OneToMany(mappedBy = "board",cascade = {CascadeType.PERSIST,CascadeType.REMOVE})
+    private List<Photo> photo = new ArrayList<>();
 
     private String location;
 
     @Enumerated(EnumType.STRING)
     private BoardStatus boardStatus;
+
+    @Builder
+    public Board(Member member,String title,String content,String location){
+        this.member= member;
+        this.title=title;
+        this.content = content;
+        this.location=location;
+        this.boardStatus=BoardStatus.POSTING;
+    }
+
+    public void update(String title,String content){
+        this.title=title;
+        this.content=content;
+    }
+
+    public void addPhoto(Photo photo){
+        this.photo.add(photo);
+        if(photo.getBoard() != this){
+            photo.setBoard(this);
+        }
+    }
 
 }
