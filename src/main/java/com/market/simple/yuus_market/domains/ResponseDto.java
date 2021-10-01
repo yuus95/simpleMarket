d79fdf685c0ba42863c3411f0e147d81368940cf
yuus95 <kkad45@naver.com>
2021-10-01 10:15:43 +0900

    [add] Response Entity구현 ResponseDto.java - 응답을 정리해서 보내기위한 ResponseDto 구현

---
+++ src/main/java/com/market/simple/yuus_market/domains/ResponseDto.java
@@ -0,0 +1,15 @@
+package com.market.simple.yuus_market.domains;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class ResponseDto<T> {
+    private int code;
+    private String message;
+    private T data;
+}
