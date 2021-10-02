2be5df2ebac6cd4f9d47bdef649584f1d5eac8c9
yuus95 <kkad45@naver.com>
2021-10-02 18:52:40 +0900

    [mod] EnableConfig 패키지 삭제 YuusMarketApplication.java - 패키지 삭제

--- src/main/java/com/market/simple/yuus_market/YuusMarketApplication.java
+++ src/main/java/com/market/simple/yuus_market/YuusMarketApplication.java
@@ -2,7 +2,6 @@ package com.market.simple.yuus_market;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
@@ -12,5 +11,4 @@ public class YuusMarketApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(YuusMarketApplication.class, args);
 	}
-
 }
