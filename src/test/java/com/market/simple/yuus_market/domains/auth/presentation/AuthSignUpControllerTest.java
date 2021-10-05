adfadb799ae5ea8c7dfb1af2fafbc63dcffd6da2
yuus95 <kkad45@naver.com>
2021-10-05 16:51:21 +0900

    [add] 회원가입 컨트롤러 테스트 코드 작성 AuthSignUpControllerTest.java - 회원가입 컨트롤러 테스트 코드 작성

---
+++ src/test/java/com/market/simple/yuus_market/domains/auth/presentation/AuthSignUpControllerTest.java
@@ -0,0 +1,59 @@
+package com.market.simple.yuus_market.domains.auth.presentation;
+
+import com.google.gson.Gson;
+import com.market.simple.yuus_market.domains.auth.application.AuthSignUpService;
+import com.market.simple.yuus_market.domains.auth.application.dto.MemberRequestDto;
+import com.market.simple.yuus_market.domains.auth.application.dto.MemberResponseDto;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(MockitoExtension.class)
+class AuthSignUpControllerTest {
+    @Mock
+    private AuthSignUpService authSignUpService;
+
+    MockMvc mockMvc;
+
+    @InjectMocks
+    private AuthSignUpController authSignUpController;
+
+
+    @BeforeEach
+    public void init() {
+        mockMvc = MockMvcBuilders.standaloneSetup(authSignUpController).build();
+    }
+
+    @Test
+    public void 회원가입_컨트롤러_테스트() throws Exception {
+        //given
+        MemberRequestDto memberRequestDto = new MemberRequestDto("테스트", "kkad45", "1234", "010-4710-5883");
+
+        MemberResponseDto memberResponseDto = new MemberResponseDto(memberRequestDto.getUserId());
+        Mockito.when(authSignUpService.signup(Mockito.any())).thenReturn(memberResponseDto);
+
+        //when
+        ResultActions perform = mockMvc.perform(MockMvcRequestBuilders.post("/signup")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(new Gson().toJson(memberRequestDto)));
+
+        //then
+
+        final MvcResult mvcResult = perform.andExpect(status().isOk()).andReturn();
+        final String token = mvcResult.getResponse().getContentAsString();
+        Assertions.assertThat(token).isNotNull();
+    }
+}
