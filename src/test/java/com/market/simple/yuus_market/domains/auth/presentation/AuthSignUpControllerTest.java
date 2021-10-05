8dfeb2ab8cb8a4f30e7be614cd651d95aec88bcc
yuus95 <kkad45@naver.com>
2021-10-05 17:15:45 +0900

    [mod] 테스트 코드 수정 AuthSignUpControllerTest.java - 코드를 간결하게 수정

--- src/test/java/com/market/simple/yuus_market/domains/auth/presentation/AuthSignUpControllerTest.java
+++ src/test/java/com/market/simple/yuus_market/domains/auth/presentation/AuthSignUpControllerTest.java
@@ -4,7 +4,6 @@ import com.google.gson.Gson;
 import com.market.simple.yuus_market.domains.auth.application.AuthSignUpService;
 import com.market.simple.yuus_market.domains.auth.application.dto.MemberRequestDto;
 import com.market.simple.yuus_market.domains.auth.application.dto.MemberResponseDto;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -14,7 +13,6 @@ import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -43,17 +41,21 @@ class AuthSignUpControllerTest {
         MemberRequestDto memberRequestDto = new MemberRequestDto("테스트", "kkad45", "1234", "010-4710-5883");
 
         MemberResponseDto memberResponseDto = new MemberResponseDto(memberRequestDto.getUserId());
-        Mockito.when(authSignUpService.signup(Mockito.any())).thenReturn(memberResponseDto);
 
         //when
-        ResultActions perform = mockMvc.perform(MockMvcRequestBuilders.post("/signup")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(new Gson().toJson(memberRequestDto)));
+        Mockito.when(authSignUpService.signup(Mockito.any())).thenReturn(memberResponseDto);
 
         //then
+        ResultActions perform = mockMvc.perform(MockMvcRequestBuilders.post("/signup")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(new Gson().toJson(memberRequestDto)))
+                .andExpect(status().isOk());
 
-        final MvcResult mvcResult = perform.andExpect(status().isOk()).andReturn();
-        final String token = mvcResult.getResponse().getContentAsString();
-        Assertions.assertThat(token).isNotNull();
+//        //then
+//        ResultActions resultActions = perform.andExpect(status().isOk());
+//
+//        final MvcResult mvcResult = perform.andExpect(status().isOk()).andReturn();
+//        final String token = mvcResult.getResponse().getContentAsString();
+//        Assertions.assertThat(token).isNotNull();
     }
 }
