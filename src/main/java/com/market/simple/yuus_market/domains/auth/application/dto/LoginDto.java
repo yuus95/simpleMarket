d586ba5eea43ee7c0eaa10b2fbd2c30088770fe4
yuus95 <kkad45@naver.com>
2021-10-07 14:49:10 +0900

    [mod] loginDto Setter추가 LoginDto.java - 테스트코드를 위한 setter추가

--- src/main/java/com/market/simple/yuus_market/domains/auth/application/dto/LoginDto.java
+++ src/main/java/com/market/simple/yuus_market/domains/auth/application/dto/LoginDto.java
@@ -1,12 +1,13 @@
 package com.market.simple.yuus_market.domains.auth.application.dto;
 
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 
-import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 
 @Getter
+@Setter
 public class LoginDto {
     @NotBlank(message = "아이디는 필수입니다.")
     private String userId;
