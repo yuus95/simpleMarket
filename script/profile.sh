6558fec48ceaf8f28cb646bebbdb1cedd320b8d7
yuus95 <kkad45@naver.com>
2021-10-30 17:41:11 +0900

    [mod] 오타수정

--- script/profile.sh
+++ script/profile.sh
@@ -1,43 +1,38 @@
 #!/usr/bin/env bash
 
-# bash 스크립트는 값을 반환하는 기능이 없다. echo로 결과를 출력 후 클라이언트에서 그 값을 잡아서 사용한다. 중간에 echo를 사용하면 안된다.
+# bash는 return value가 안되니 *제일 마지막줄에 echo로 해서 결과 출력*후, 클라이언트에서 값을 사용한다
 
-# 쉬고 있는 profile 찾기: real1이 사용 중이면 real2가 쉬고 있고, 반대면 real1이 쉬고 있음
-
+# 쉬고 있는 profile 찾기: real1이 사용중이면 real2가 쉬고 있고, 반대면 real1이 쉬고 있음
 function find_idle_profile()
 {
-  RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://localhost/profile)
+    RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://localhost/profile)
 
-  if [ ${RESPONSE_CODE} -ge 400 ] # 400 보다 크면
+    if [ ${RESPONSE_CODE} -ge 400 ] # 400 보다 크면 (즉, 40x/50x 에러 모두 포함)
+    then
+        CURRENT_PROFILE=real2
+    else
+        CURRENT_PROFILE=$(curl -s http://localhost/profile)
+    fi
 
-  then
-    CURRENT_PROFILE=real2
-  else
-    CURRENT_PROFILE= $(curl -s http://localhost/profile)find_idle_port
-  fi
+    if [ ${CURRENT_PROFILE} == real1 ]
+    then
+      IDLE_PROFILE=real2
+    else
+      IDLE_PROFILE=real1
+    fi
 
-  if [ ${CURRENT_PROFIE} == real1 ]
-
-  then
-    IDLE_PROFILE=real2
-  else
-    IDLE_PROFILE=real1
-  fi
-
-  echo "${IDLE_PROFILE}"
+    echo "${IDLE_PROFILE}"
 }
 
-# 쉬고 있는 profile prot 찾기
-
+# 쉬고 있는 profile의 port 찾기
 function find_idle_port()
 {
-  IDLE_PROFILE=$(find_idle_profile)
+    IDLE_PROFILE=$(find_idle_profile)
 
-  if [ ${IDLE_PROFILE} == real1 ]
-  then
-    echo "8081"
-  else
-    echo "8082"
-  fi
-
+    if [ ${IDLE_PROFILE} == real1 ]
+    then
+      echo "8081"
+    else
+      echo "8082"
+    fi
 }
