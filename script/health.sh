7ea9f6b55fbfb51464ae90cf9ae0e0659cf9d204
yuus95 <kkad45@naver.com>
2021-11-01 12:12:56 +0900

    [mod] health.sh 수정 오타수정

--- script/health.sh
+++ script/health.sh
@@ -18,24 +18,23 @@ do
   UP_COUNT=$(echo ${RESPONSE} | grep 'real' | wc -l)
 
   if [ ${UP_COUNT} -ge 1 ]
-  then # $up)COUNT >=1 ("real" 문자열이 있는지 검증)
-    echo "> Health check 성공"
-    switch_proxy
-    break
+  then # $up_count >= 1 ("real" 문자열이 있는지 검증)
+      echo "> Health check 성공"
+      switch_proxy
+      break
   else
-    echo ">Health check의 응답을 알 수 없거나 혹은 실행 상태가 아닙니다."
-    echo "> Health check: ${RESPONSE}"
+      echo "> Health check의 응답을 알 수 없거나 혹은 실행 상태가 아닙니다."
+      echo "> Health check: ${RESPONSE}"
   fi
 
   if [ ${RETRY_COUNT} -eq 10 ]
   then
-    echo "> Health check 실패"
+    echo "> Health check 실패. "
     echo "> 엔진엑스에 연결하지 않고 배포를 종료합니다."
     exit 1
   fi
 
-  echo "> Health check 연결 실패 재시도.."
+  echo "> Health check 연결 실패. 재시도..."
   sleep 10
 done
-
 
