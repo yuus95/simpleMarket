b086281d0863493a0206a06bff84ed6d238eedb0
yuus95 <kkad45@naver.com>
2021-11-01 13:35:44 +0900

    [mod] start.sh 오타수정 -start.sh 명령어 오타 수정

--- script/start.sh
+++ script/start.sh
@@ -21,8 +21,10 @@ echo "> JAR_NAME : $JAR_NAME"
 echo "> $JAR_NAME 에 실행권한 추가"
 chmod +x $JAR_NAME
 
-echo "> $JAR_NAME 를 profile=$IDLE_PROFILE 로 실행합니다."
 
 IDLE_PROFILE=$(find_idle_profile)
+echo "> $JAR_NAME 를 profile=$IDLE_PROFILE 로 실행합니다."
 
-nohup java -jar -Dspring.profiles.active=$IDLE_PROFILE $JAR_NAME >$REPOSITORY/dep2/nohup.out 2>&1 &
+nohup java -jar \
+      -Dspring.profiles.active=$IDLE_PROFILE \
+       $JAR_NAME >$REPOSITORY/dep2/nohup.out 2>&1 &
