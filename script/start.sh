ac6f98eff8cafa494ac7bab12799ca4014e2d758
yuus95 <kkad45@naver.com>
2021-11-01 12:18:03 +0900

    [mod] start.sh 수정 오타수정

--- script/start.sh
+++ script/start.sh
@@ -9,7 +9,7 @@ PROJECT_NAME=simpleMarket2
 
 echo "> Build 파일 복사"
 echo  "> $REPOSITORY/$PROJECT_NAME/build/libs/*.jar $REPOSITORY/dep2"
-cp $REPOSITORY/$PROJECT_NAME/build/libs/*.jar $REPOSITORY/dep2
+cp $REPOSITORY/$PROJECT_NAME/*.jar $REPOSITORY/dep2
 
 echo "> 새애플리케이션 배포"
 JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
