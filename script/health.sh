9b1f8ea84e1490595912577de6ea7d05d476d856
yuus95 <kkad45@naver.com>
2021-11-01 13:16:28 +0900

    [mod] switch.sh,health.sh 오타수정 health.sh start.sh 명령어 오타 수정

--- script/health.sh
+++ script/health.sh
@@ -37,4 +37,3 @@ do
   echo "> Health check 연결 실패. 재시도..."
   sleep 10
 done
-
--- script/start.sh
+++ script/start.sh
@@ -21,8 +21,8 @@ echo "> JAR_NAME : $JAR_NAME"
 echo "> $JAR_NAME 에 실행권한 추가"
 chmod +x $JAR_NAME
 
-echo "> $JAR_NAME 실행"
+echo "> $JAR_NAME 를 profile=$IDLE_PROFILE 로 실행합니다."
 
 IDLE_PROFILE=$(find_idle_profile)
 
-nohup java -jar -Dspring.profiles.active=$IDLE_PROFILE $REPOSITORY/dep2/$JAR_NAME >$REPOSITORY/dep/nohup.out 2>&1 &
+nohup java -jar -Dspring.profiles.active=$IDLE_PROFILE $REPOSITORY/dep2/$JAR_NAME >$REPOSITORY/dep2/nohup.out 2>&1 &
